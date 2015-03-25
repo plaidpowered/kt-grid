824be9433209bb9fdec27ed17dcb2e5627808f77
Paul Houser <paul@aja>
2015-03-25 17:51:54 -0400

    Completed functionality
    - Added `col` preprocessor
    - Completely rewrote `row` preprocessor based on `col` pp function

--- kt-grid.php
+++ kt-grid.php
@@ -3,51 +3,12 @@
 kirbytext::$pre[] = function($kirbytext, $value) 
 {
     
-  preg_match_all("/^\s*--\s*row\s*(xs|sm|md|lg)?\s*([\w\,\s]*)\s*--\s*$/m",$value,$row_matches,PREG_OFFSET_CAPTURE|PREG_SET_ORDER);
-  preg_match_all("/^\s*---\s*$/m",$value,$section_matches,PREG_OFFSET_CAPTURE|PREG_SET_ORDER);
-  preg_match_all("/^\s*--\s*end\s*--\s*$/m",$value,$end_matches,PREG_OFFSET_CAPTURE|PREG_SET_ORDER);
+  $value = ktgrid_preprocess_col($value);
+  $value = ktgrid_preprocess_row($value);
   
-  $value_length = strlen($value);
-  //$global_offset = 0;
-    
-  foreach($row_matches as $row)
-  {
-    $beginning = $row[0][1];
-    
-    $end = null;
-    $offset = $beginning;    
-    while($offset <= $beginning && count($end_matches)) 
-    {
-      $end = array_shift($end_matches);
-      $offset = $end[0][1];
-    }
-        
-    $sections = [];
-    $offset = $beginning;
-    while($offset < $end[0][1] && count($section_matches))     
-    {      
-      $section = array_shift($section_matches);
-      if ($section[0][1] > $beginning)
-        $sections[] = $section;
-      $offset = $section[0][1];
-    }
-        
-    $grid_sizes = explode(",",$row[2][0]);
-    $grid_target = empty($row[1][0]) ? "md" : $row[1][0];
-    
-    if (is_null($end) || count($grid_sizes)-1 !== count($sections))
-      return $value; 
-      // something is broken
-        
-    $value = preg_replace("/{$row[0][0]}/m", "<!--bs|row-->\n<!--bs|{$grid_target}:".trim($grid_sizes[0])."-->", $value, 1);
-    foreach($sections as $index => $section)
-      $value = preg_replace("/{$section[0][0]}/m","<!--bs|end-->\n<!--bs|{$grid_target}:".trim($grid_sizes[$index+1])."-->",$value,1);
-    $value = preg_replace("/{$end[0][0]}/m","<!--bs|end-->",$value,1);
-    
-    //$global_offset = strlen($value) - $value_length;
-  }
   
   return $value;
+  
 };
 
 kirbytext::$post[] = function($kirbytext, $value) 
@@ -68,10 +29,14 @@ kirbytext::$post[] = function($kirbytext, $value)
     {
       $replacement = '</div>';
     }
+    else if (count($match) >= 2 && $match[1] === "cf")
+    {
+      $replacement = '<div class="clearfix"></div>';
+    }
     else if (count($match) >= 4 && !empty($match[1]) && is_numeric($match[3]))
     {
       $replacement = sprintf('<div class="col-%1$s-%2$d%3$s">', $match[1], $match[3],
-                             count($match) >= 6 && is_numeric($match[5]) ? sprintf("col-%s-offset-%d", $match[1], $match[5]) : "" );
+                             count($match) >= 6 && is_numeric($match[5]) ? sprintf(" col-%s-offset-%d", $match[1], $match[5]) : "" );
     }
     else if (count($match) >= 6 && !empty($match[1]) && is_numeric($match[5]))
     {
@@ -85,3 +50,70 @@ kirbytext::$post[] = function($kirbytext, $value)
   return $value;
   
 };
+
+function ktgrid_preprocess_col($content) 
+{
+
+  while( preg_match("/(\s*--\s*col\s*(xs|sm|md|lg)?\s*([0-9]{1,2}\s*)?(\+([0-9]{1,2}))?\s*--\s*)(.*?)(\s*--\s*end\s*--\s*)/ms", $content, $match, PREG_OFFSET_CAPTURE) )
+  {
+      
+    if (count($match) !== 8)
+      return $content;
+
+    $inside = $match[6][0];
+
+    $mq_target = empty($match[2][0]) ? "md" : $match[2][0];
+    $col_size = empty($match[3][0]) ? null : trim($match[3][0]);
+
+    $offset = empty($match[5][0]) ? null : trim($match[5][0]);
+
+    $replacement = sprintf("\n<!--bs|%s%s%s-->\n%s\n<!--bs|end-->\n",
+                           $mq_target,
+                           !is_null($col_size) ? ":$col_size" : "",
+                           !is_null($offset)   ? "+$offset"   : "",
+                           $inside);
+
+    $content = substr_replace($content, $replacement, $match[0][1], strlen($match[0][0]));
+    
+  }
+  
+  return $content;
+  
+}
+
+function ktgrid_preprocess_row($content)
+{
+  
+  while ( preg_match("/(\s*--\s*row\s*(xs|sm|md|lg)?\s*([0-9\,\s]*)\s*--\s*)(.*?)(\s*--\s*end\s*--\s*)/ms", $content, $match, PREG_OFFSET_CAPTURE) ) 
+  {
+    if (count($match) !== 6)
+      return $content;
+      //what the heck happened here
+
+    $inside = $match[4][0];
+    
+    $mq_target = empty($match[2][0]) ? "md" : $match[2][0];
+    $columns = explode(",",$match[3][0]);
+    if (!$columns || count($columns)<1)
+      return $content;
+      //not even sure if this is possible
+    
+    preg_match_all("/^\s*---\s*$/m",$inside,$sections,PREG_OFFSET_CAPTURE|PREG_SET_ORDER);
+    if (count($sections)+1 !== count($columns))
+      return $content;
+      //number of sections don't match specified columns
+    
+    $replacement = "<!--bs|row-->\n<!--bs|{$mq_target}:".trim($columns[0])."-->\n" . $inside;
+    
+    foreach($sections as $index => $section) 
+      $replacement = preg_replace("/{$section[0][0]}/m", "<!--bs|end-->\n<!--bs|{$mq_target}:".trim($columns[$index+1])."-->", $replacement, 1);    
+                                  
+    $replacement .= "\n<!--end-->\n<!--bs|end-->\n<!--bs|cf-->\n";
+               
+    $content = substr_replace($content, $replacement, $match[0][1], strlen($match[0][0]));
+    
+  }
+  
+  return $content;
+  
+}
